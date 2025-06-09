345685b61d38f30f9f95d129cdef1371a52c4e4c
git-mk39 <64092501+git-mk39@users.noreply.github.com>
2025-06-09 14:03:00 +0700

    update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
+import epimap from "./api/epimap.route.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/v1/epimap", epimap);
 app.use("*", (req, res) => {
   res.status(404).json({ error: "not found" });
 });
