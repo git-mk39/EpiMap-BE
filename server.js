db4d8d625815ee8b9eec421b1acc68b32c47583b
git-mk39 <64092501+git-mk39@users.noreply.github.com>
2025-06-09 14:08:36 +0700

    modify server.js

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/v1/epimap", epimap);
-app.use("*", (req, res) => {
-  res.status(404).json({ error: "not found" });
-});
+app.use("/", epimap);
+// app.use("*", (req, res) => {
+//   res.status(404).json({ error: "not found" });
+// });
 
 export default app;
